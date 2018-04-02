ba243bc27d4cc9c1c9e429399089eb411b72a199
Heinz <araqueheinz@gmail.com>
2018-04-02 14:02:10 -0400

    adding the project

---
+++ js/index.js
@@ -0,0 +1,102 @@
+
+
+const KEY = `2055f069ebab88a96da56841e6da6b78`;
+const imageURL = `https://image.tmdb.org/t/p/w500/`;
+const urlPopularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=${KEY}&language=en-US&page=1`;
+const urlTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${KEY}&language=en-US&page=1`;
+
+let popularMovies = document.querySelector('#most-popular-movies');
+let featuredMoives = document.querySelector('#featured-movies');
+let topRatedMovies = document.querySelector('#top-rated');
+
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                        MOST POPULAR MOVIES                          / */
+/* /////////////////////////////////////////////////////////////////////// */
+
+
+fetch(urlPopularMovies)
+.then(items => items.json())
+.then(items => {
+   
+    let content = '<ul>';
+    console.log(items.results);
+    
+   content += `
+   <li>
+   <h2>${items.results[0].original_title}</h2>
+   <img src="${imageURL}${items.results[0].backdrop_path}" alt="${items.results[0].original_title}">
+   </li>
+   <li>
+   <h2>${items.results[1].original_title}</h2>
+   <img src="${imageURL}${items.results[1].backdrop_path}" alt="${items.results[0].original_title}">
+   </li>
+   </ul>
+   `;
+   
+   popularMovies.innerHTML = content;
+   })
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                       FEATURED MOVIES                               / */
+/* /////////////////////////////////////////////////////////////////////// */
+    
+fetch(urlPopularMovies)
+.then(items => items.json())
+.then(items => {
+   
+    let content = '<ul>';
+    
+    
+   content += `
+   <li>
+   <h2>${items.results[2].original_title}</h2>
+   <img src="${imageURL}${items.results[2].backdrop_path}" alt="${items.results[2].original_title}">
+   </li>
+   <li>
+   <h2>${items.results[3].original_title}</h2>
+   <img src="${imageURL}${items.results[3].backdrop_path}" alt="${items.results[3].original_title}">
+   </li>
+   <li>
+   <h2>${items.results[4].original_title}</h2>
+   <img src="${imageURL}${items.results[4].backdrop_path}" alt="${items.results[4].original_title}">
+   </li>
+   <li>
+   <h2>${items.results[5].original_title}</h2>
+   <img src="${imageURL}${items.results[5].backdrop_path}" alt="${items.results[5].original_title}">
+   </li>
+   </ul>
+   `;
+   
+   featuredMoives.innerHTML = content;
+   })
+
+/* /////////////////////////////////////////////////////////////////////// */
+/* /                       TOP RATED MOVIES                              / */
+/* /////////////////////////////////////////////////////////////////////// */
+
+fetch(urlTopRated)
+.then(items => items.json())
+.then(items => {
+    console.log(items.results);
+    let content = `
+    <h1>Top Rated</h1>
+    <ul>`;
+    
+    items.results.forEach(element =>{
+        content += `
+        <li>
+        <img src="${imageURL}${element.poster_path}" alt="${element.original_title}">
+        
+        </li>
+        `
+    })
+
+     content += '</ul>';
+   
+    topRatedMovies.innerHTML = content;
+   })
+
+    
+
+// data to the #results section
---
+++ readme.md
@@ -0,0 +1,6 @@
+ Heinz Araque
+ Project: Build an API Driven Search App
+
+
+ Website link for the website:
+ https://araqueheinz.github.io/AraqueHeinz_DWS2_Project/
